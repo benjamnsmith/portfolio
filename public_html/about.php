87fcc5a88f549aac79572eb42928c1667b64f780
Ben Smith <benjaminwrobelsmith@gmail.com>
2023-03-08 00:46:53 -0600

    updated about page contents and styling

--- public_html/about.php
+++ public_html/about.php
@@ -25,40 +25,41 @@
     </section>
     <div class="line"></div>
     <section id="content">
-        <img src="./img/me-autumn.jpg" />
-        <p class="about">My name is Ben and I study computer systems at the University of Minnesota - Twin Cities.
-            I am fascinated by the way our systems are built, from the enterprise intranets to the world wide web as a
-            whole.
-            Technology is changing at a rapid pace now more than ever, and continuing to design apps etc. requires a
-            deep understanding of the underlying systems upon which they're built, and their seurity.
-            I really enjoy reading submissions from the annual <a
-                href="https://www.usenix.org/publications/proceedings" target="_blank">USENIX</a> conference, as well as the
-            content posted by <a href="https://googleprojectzero.blogspot.com/" target="_blank">Google's Project Zero</a>.</p>
-        <p class="about"> Outside of tech, I'm interested in languages
-            and cultures of the world. I study Russian and French in addition to CS, and spent my Fall 2022 semester in
-            Montpellier, France. There, I studied linguisitics and its appilcations, and taught English to elementary school
-            students.
-        </p>
-        <p class="about">
-            I'm excited about computer systems and security, and I want everyone else to be too! I am an undergraduate teaching
-            assistant for a computer systems class aimed at upperclassmen computer science minors or MIS students. I create weekly labs
-            for them with the goal of exploring C, memory management, assembly code, and more. <br>
-            I also volunteer with the Hennepin County Library where I help patrons with basic computer skills (typing, web browsing, printing) as well
-            as with job applications and other online tasks. 
-        </p> 
-        <p class="quick_facts">
-        I'm interested in any programming language that allows me to hack together what I'm looking to achieve. I mostly use <span>Python</span> 
-            (<span class="subtype">Apache Thrift</span>, <span class="subtype">Flask</span>), but recently have been intrigued by <span>JavaScript</span>. 
-            This website is written almost completely in <span>PHP</span>. In school, <span>C</span> was my language of choice. 
-            <br>
-            <br>
-            I've recently become geeked about HTB and would love to share my experience there with you. Take a peek at my 
-            <a href="https://app.hackthebox.com/profile/1415198" target="_blank">HTB profile</a> or 
-            see some of my <a href="#">machine writeups</a>. 
-            <br>
-            <br>
-            In Feb 2023 my team and I won Deloitte's annual Cyber Threat Competition!    
-        </p>
+        <div class="col1">
+            <p>&emsp;&emsp;&emsp;&emsp;I'm Ben, a computer systems student at the University of Minnesota - Twin Cities. I'm fascinated 
+                by the construction of modern systems, from enterprise intranets to the world wide web. Keeping up with the latest 
+                technology is essential, and I'm passionate about exploring recent developments in computer security through sources 
+                like the <a href="https://www.usenix.org/publications/proceedings" target="_blank">USENIX</a> conference and 
+                <a href="https://googleprojectzero.blogspot.com/" target="_blank">Google's Project Zero</a>.</p>
+
+            <p>&emsp;&emsp;&emsp;&emsp;Aside from my studies, I enjoy learning about different cultures and languages. I speak Russian 
+                and French and had the chance to teach English to elementary school students during my time studying in Montpellier, France 
+                during Fall 2022.
+            </p>
+            
+            <p>
+                &emsp;&emsp;&emsp;&emsp;As an undergraduate teaching assistant, I create and run weekly labs for upperclassmen computer science minors 
+                and MIS students on topics around computer systems focusing on C, x86_64, and memory management. I also volunteer with the Hennepin County 
+                Library, helping patrons with basic computer skills and online tasks to provide access to digital resources.
+            </p>
+        </div>
+        <div class="col2">
+            <img src="./img/me-autumn.jpg" />
+            <p class="quick_facts">
+            I'm interested in any programming language that allows me to hack together what I'm looking to achieve. I mostly use <span>Python</span>,
+             but recently have been intrigued by <span>JavaScript</span>. 
+                This website is written almost completely in <span>PHP</span>. I'm "classially trained," as it were, in <span>C</span>. 
+                <br>
+                <br>
+                <a href="https://app.hackthebox.com/profile/1415198" target="_blank">HTB profile</a> 
+                and <a href="#">machine writeups</a>. 
+                <br>
+                <br>
+                In Feb 2023 my team and I won Deloitte's annual 
+                <a href="https://www.linkedin.com/posts/benjamnsmith_i-spent-the-past-weekend-in-westlake-tx-activity-7037083343682736128-jVmF" target="_blank">Cyber Threat Competition!</a>    
+            </p>
+        </div>
+        
 
     </section>
     <?php
