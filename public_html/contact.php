90ccedc9c6a8da7b86d89c7c284b65695fa07df5
Ben Smith <benjaminwrobelsmith@gmail.com>
2023-03-10 12:04:11 -0600

    added simple contact page

---
+++ public_html/contact.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Oxygen&family=Oxygen+Mono&display=swap" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" rel="stylesheet" />
+    <link href="./css/generic.css" rel="stylesheet">
+    <link href="./css/contact.css" rel="stylesheet">
+    
+    <title>benjamnsmith - Contact</title>
+</head>
+<body>
+    <?php
+    
+    include "navbar.html";
+    
+    ?>
+    <section id="intro">
+        <p class="name"><span>Contact</span></p>
+  </section>
+  <div class="line"></div>
+  <section class="content">
+    <div class="info">
+        <h2>Email me</h2>
+        <p>smit9524@umn.edu</p>
+    </div>
+  </section>
+  <?php
+    
+    include "footer.html";
+    
+    ?>
+</body>
+</html>
